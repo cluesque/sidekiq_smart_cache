26e90034c01e5abb18ac64233845dcd45a0ffd30
Bill Kirtley <bill.kirtley@gmail.com>
2020-02-04 23:17:16 -0500

    Some behavior for the test model

--- test/dummy/app/models/doohickey.rb
+++ test/dummy/app/models/doohickey.rb
@@ -1,2 +1,12 @@
 class Doohickey < ApplicationRecord
+  def self.do_a_thing
+    doohickey = create(name: "narf #{count + 1}")
+    logger.info "Performing #{doohickey.name}"
+    sleep 2.0
+    logger.info "Completed thing #{doohickey.name}"
+    doohickey.name
+  end
+  def make_a_thing(arg)
+    "#{name} #{arg}"
+  end
 end
