185a308a36d23388477b859d2014a356d3cdd6a1
Bill Kirtley <bill.kirtley@gmail.com>
2020-02-18 21:21:19 -0500

    Better support of inline
    Sometimes you don't want a separate sidekiq process

--- lib/sidekiq_smart_cache/redis.rb
+++ lib/sidekiq_smart_cache/redis.rb
@@ -14,6 +14,7 @@ module SidekiqSmartCache
     end
 
     def wait_for_done_message(key, timeout)
+      return true if defined?(Sidekiq::Testing) && Sidekiq::Testing.inline?
       if brpop(job_completion_key(key), timeout.to_i)
         # log_msg("got done message for #{key}")
         send_done_message(key) # put it back for any other readers
